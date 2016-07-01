ee422e8d96a479bfb814fbeb8db750fd651cf838
t_satou <t_satou@aoi-seiki.co.jp>
2016-07-01 14:34:59 +0900

    first commit

---
+++ HsvColorPicker/frm_HsvColorpicker.Designer.vb
@@ -0,0 +1,204 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frm_HsvColorpicker
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナで必要です。
+    'Windows フォーム デザイナを使用して変更できます。  
+    'コード エディタを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+    Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frm_HsvColorpicker))
+    Me.GroupBox1 = New System.Windows.Forms.GroupBox
+    Me.Label4 = New System.Windows.Forms.Label
+    Me.pic_v = New System.Windows.Forms.PictureBox
+    Me.pic_s = New System.Windows.Forms.PictureBox
+    Me.pic_h = New System.Windows.Forms.PictureBox
+    Me.lblColor = New System.Windows.Forms.Label
+    Me.Label3 = New System.Windows.Forms.Label
+    Me.Label2 = New System.Windows.Forms.Label
+    Me.lblRgbValue = New System.Windows.Forms.Label
+    Me.lbl_V = New System.Windows.Forms.Label
+    Me.lbl_S = New System.Windows.Forms.Label
+    Me.lbl_H = New System.Windows.Forms.Label
+    Me.Label1 = New System.Windows.Forms.Label
+    Me.sbar_V = New System.Windows.Forms.HScrollBar
+    Me.sbar_S = New System.Windows.Forms.HScrollBar
+    Me.sbar_H = New System.Windows.Forms.HScrollBar
+    Me.btnClose = New System.Windows.Forms.Button
+    Me.btnOk = New System.Windows.Forms.Button
+    Me.GroupBox1.SuspendLayout()
+    CType(Me.pic_v, System.ComponentModel.ISupportInitialize).BeginInit()
+    CType(Me.pic_s, System.ComponentModel.ISupportInitialize).BeginInit()
+    CType(Me.pic_h, System.ComponentModel.ISupportInitialize).BeginInit()
+    Me.SuspendLayout()
+    '
+    'GroupBox1
+    '
+    Me.GroupBox1.Controls.Add(Me.Label4)
+    Me.GroupBox1.Controls.Add(Me.pic_v)
+    Me.GroupBox1.Controls.Add(Me.pic_s)
+    Me.GroupBox1.Controls.Add(Me.pic_h)
+    Me.GroupBox1.Controls.Add(Me.lblColor)
+    Me.GroupBox1.Controls.Add(Me.Label3)
+    Me.GroupBox1.Controls.Add(Me.Label2)
+    Me.GroupBox1.Controls.Add(Me.lblRgbValue)
+    Me.GroupBox1.Controls.Add(Me.lbl_V)
+    Me.GroupBox1.Controls.Add(Me.lbl_S)
+    Me.GroupBox1.Controls.Add(Me.lbl_H)
+    Me.GroupBox1.Controls.Add(Me.Label1)
+    Me.GroupBox1.Controls.Add(Me.sbar_V)
+    Me.GroupBox1.Controls.Add(Me.sbar_S)
+    Me.GroupBox1.Controls.Add(Me.sbar_H)
+    resources.ApplyResources(Me.GroupBox1, "GroupBox1")
+    Me.GroupBox1.Name = "GroupBox1"
+    Me.GroupBox1.TabStop = False
+    '
+    'Label4
+    '
+    resources.ApplyResources(Me.Label4, "Label4")
+    Me.Label4.Name = "Label4"
+    '
+    'pic_v
+    '
+    resources.ApplyResources(Me.pic_v, "pic_v")
+    Me.pic_v.Name = "pic_v"
+    Me.pic_v.TabStop = False
+    '
+    'pic_s
+    '
+    resources.ApplyResources(Me.pic_s, "pic_s")
+    Me.pic_s.Name = "pic_s"
+    Me.pic_s.TabStop = False
+    '
+    'pic_h
+    '
+    resources.ApplyResources(Me.pic_h, "pic_h")
+    Me.pic_h.Name = "pic_h"
+    Me.pic_h.TabStop = False
+    '
+    'lblColor
+    '
+    Me.lblColor.BackColor = System.Drawing.Color.White
+    Me.lblColor.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+    resources.ApplyResources(Me.lblColor, "lblColor")
+    Me.lblColor.Name = "lblColor"
+    '
+    'Label3
+    '
+    resources.ApplyResources(Me.Label3, "Label3")
+    Me.Label3.Name = "Label3"
+    '
+    'Label2
+    '
+    resources.ApplyResources(Me.Label2, "Label2")
+    Me.Label2.Name = "Label2"
+    '
+    'lblRgbValue
+    '
+    resources.ApplyResources(Me.lblRgbValue, "lblRgbValue")
+    Me.lblRgbValue.Name = "lblRgbValue"
+    '
+    'lbl_V
+    '
+    resources.ApplyResources(Me.lbl_V, "lbl_V")
+    Me.lbl_V.Name = "lbl_V"
+    '
+    'lbl_S
+    '
+    resources.ApplyResources(Me.lbl_S, "lbl_S")
+    Me.lbl_S.Name = "lbl_S"
+    '
+    'lbl_H
+    '
+    resources.ApplyResources(Me.lbl_H, "lbl_H")
+    Me.lbl_H.Name = "lbl_H"
+    '
+    'Label1
+    '
+    resources.ApplyResources(Me.Label1, "Label1")
+    Me.Label1.Name = "Label1"
+    '
+    'sbar_V
+    '
+    resources.ApplyResources(Me.sbar_V, "sbar_V")
+    Me.sbar_V.Maximum = 264
+    Me.sbar_V.Name = "sbar_V"
+    Me.sbar_V.Value = 1
+    '
+    'sbar_S
+    '
+    resources.ApplyResources(Me.sbar_S, "sbar_S")
+    Me.sbar_S.Maximum = 264
+    Me.sbar_S.Name = "sbar_S"
+    Me.sbar_S.Value = 1
+    '
+    'sbar_H
+    '
+    resources.ApplyResources(Me.sbar_H, "sbar_H")
+    Me.sbar_H.Maximum = 264
+    Me.sbar_H.Name = "sbar_H"
+    Me.sbar_H.Value = 1
+    '
+    'btnClose
+    '
+    resources.ApplyResources(Me.btnClose, "btnClose")
+    Me.btnClose.Name = "btnClose"
+    Me.btnClose.UseVisualStyleBackColor = True
+    '
+    'btnOk
+    '
+    resources.ApplyResources(Me.btnOk, "btnOk")
+    Me.btnOk.Name = "btnOk"
+    Me.btnOk.UseVisualStyleBackColor = True
+    '
+    'frm_HsvColorpicker
+    '
+    resources.ApplyResources(Me, "$this")
+    Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+    Me.ControlBox = False
+    Me.Controls.Add(Me.btnOk)
+    Me.Controls.Add(Me.btnClose)
+    Me.Controls.Add(Me.GroupBox1)
+    Me.Name = "frm_HsvColorpicker"
+    Me.GroupBox1.ResumeLayout(False)
+    Me.GroupBox1.PerformLayout()
+    CType(Me.pic_v, System.ComponentModel.ISupportInitialize).EndInit()
+    CType(Me.pic_s, System.ComponentModel.ISupportInitialize).EndInit()
+    CType(Me.pic_h, System.ComponentModel.ISupportInitialize).EndInit()
+    Me.ResumeLayout(False)
+
+  End Sub
+  Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+  Friend WithEvents lblColor As System.Windows.Forms.Label
+  Friend WithEvents Label3 As System.Windows.Forms.Label
+  Friend WithEvents Label2 As System.Windows.Forms.Label
+  Friend WithEvents lbl_V As System.Windows.Forms.Label
+  Friend WithEvents lbl_S As System.Windows.Forms.Label
+  Friend WithEvents lbl_H As System.Windows.Forms.Label
+  Friend WithEvents Label1 As System.Windows.Forms.Label
+  Friend WithEvents sbar_V As System.Windows.Forms.HScrollBar
+  Friend WithEvents sbar_S As System.Windows.Forms.HScrollBar
+  Friend WithEvents sbar_H As System.Windows.Forms.HScrollBar
+  Friend WithEvents pic_h As System.Windows.Forms.PictureBox
+  Friend WithEvents pic_s As System.Windows.Forms.PictureBox
+  Friend WithEvents pic_v As System.Windows.Forms.PictureBox
+  Friend WithEvents Label4 As System.Windows.Forms.Label
+  Friend WithEvents lblRgbValue As System.Windows.Forms.Label
+  Friend WithEvents btnClose As System.Windows.Forms.Button
+  Friend WithEvents btnOk As System.Windows.Forms.Button
+End Class
---
+++ HsvColorPicker/frm_HsvColorpicker.vb
@@ -0,0 +1,306 @@
+﻿
+Public Class frm_HsvColorpicker
+
+  Private _hsb As New Hsv
+  Private _isCancel As Boolean = False
+
+  Private h, s, v As Double
+
+  Public Shadows Function Show() As Hsv
+    MyBase.Show()
+
+    If _isCancel Then
+      Return Nothing
+    Else
+      Return _hsb
+    End If
+  End Function
+
+  Public Shadows Function ShowDialog(Optional ByVal h As Double = 0, Optional ByVal s As Double = 0, Optional ByVal v As Double = 0) As Hsv
+
+    Me.h = h
+    Me.s = s
+    Me.v = v
+    Call PaintColorViewControl(h, s, v)
+
+    MyBase.ShowDialog()
+
+    If _isCancel Then
+      Return Nothing
+    Else
+      Return _hsb
+    End If
+
+  End Function
+
+  Private Sub frm_HsvColorpicker_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+    _isCancel = False
+  End Sub
+  Private Sub frm_HsvColorpicker_Shown(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Shown
+
+    lbl_H.Text = h
+    lbl_S.Text = s
+    lbl_V.Text = v
+    sbar_H.Value = h
+    sbar_S.Value = s
+    sbar_V.Value = v
+
+    Call PaintColorViewControl(Val(lbl_H.Text), Val(lbl_S.Text), Val(lbl_V.Text))
+    Me.Update()
+  End Sub
+
+
+  Private Sub sbar_H_ValueChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles sbar_V.ValueChanged, sbar_S.ValueChanged, sbar_H.ValueChanged
+
+    Select Case sender.name
+      Case sbar_H.Name
+        lbl_H.Text = sbar_H.Value
+      Case sbar_S.Name
+        lbl_S.Text = sbar_S.Value
+      Case sbar_V.Name
+        lbl_V.Text = sbar_V.Value
+    End Select
+
+    'Call DrawAllowScroll(Val(sbar_H.Value))
+
+    'width->255->360 諧調変換
+    Dim _8bit_val_h As Integer = CInt(Hsv.Unit(Val(lbl_H.Text), 255, 360))
+
+    lblColor.BackColor = Hsv.HsvToColor(_8bit_val_h, Val(lbl_S.Text), Val(lbl_V.Text))
+    Call PaintColorViewControl(Val(lbl_H.Text), Val(lbl_S.Text), Val(lbl_V.Text))
+
+    lblRgbValue.Text = lblColor.BackColor.R.ToString("X2") & _
+                       lblColor.BackColor.G.ToString("X2") & _
+                       lblColor.BackColor.B.ToString("X2") & "(" & _
+                       lblColor.BackColor.R.ToString.PadLeft(3) & "," & _
+                       lblColor.BackColor.G.ToString.PadLeft(3) & "," & _
+                       lblColor.BackColor.B.ToString.PadLeft(3) & ")"
+
+  End Sub
+
+  Sub DrawAllowScroll(ByVal offset As Double)
+
+    offset = Hsv.Unit(offset, 255, sbar_H.Width - 40)
+
+    Dim sendervalue_w As Integer = sbar_H.Location.X + 15 + offset
+    Dim sendervalue_h As Integer = sbar_H.Location.Y - 10
+
+
+    Dim g As Graphics = Me.GroupBox1.CreateGraphics
+    Dim myBrush As Brush = System.Drawing.SystemBrushes.Control
+    g.FillRectangle(myBrush, sendervalue_w - 8, sendervalue_h, sendervalue_h + 10, sendervalue_h + 10)       ' clear right hand slider
+
+    Dim pencil As New Pen(Color.FromArgb(116, 114, 106))    ' Same gray Photoshop uses
+    Dim WhiteBrush As Brush = Brushes.White
+
+    Dim UpArrow As Point() = _
+    { _
+      New Point(0 + sendervalue_w, 0 + sendervalue_h), _
+      New Point(0 + sendervalue_w, 1 + sendervalue_h), _
+      New Point(0 + sendervalue_w, 5 + sendervalue_h), _
+      New Point(4 + sendervalue_w, 9 + sendervalue_h), _
+      New Point(8 + sendervalue_w, 5 + sendervalue_h), _
+      New Point(8 + sendervalue_w, 0 + sendervalue_h), _
+      New Point(0 + sendervalue_w, 0 + sendervalue_h) _
+    }
+
+    g.FillPolygon(WhiteBrush, UpArrow)  ' Fill left arrow with white
+    g.DrawPolygon(pencil, UpArrow)    ' Draw left arrow border with gray
+
+  End Sub
+
+  Sub PaintColorViewControl(ByVal val_h As Integer, ByVal val_s As Integer, ByVal val_v As Integer)
+
+    Dim h_bmp As New Bitmap(pic_h.Width, pic_h.Height)
+    Dim s_bmp As New Bitmap(pic_s.Width, pic_s.Height)
+    Dim v_bmp As New Bitmap(pic_v.Width, pic_v.Height)
+
+    'width->255->360 諧調変換
+    Dim _8bit_val_h As Integer = CInt(Hsv.Unit(val_h, 255, 360))
+
+
+    'H:Hue 描画
+    For i As Integer = 0 To pic_h.Width - 1
+
+      'width->255->360 諧調変換
+      Dim _8bit As Integer = CInt(Hsv.Unit(i, pic_h.Width, 255))
+      Dim rad As Integer = CInt(Hsv.Unit(_8bit, 255, 360))
+
+      Dim c As Color = Hsv.HsvToColor(rad, 255, 255)
+
+      For h As Integer = 0 To h_bmp.Height - 1
+        h_bmp.SetPixel(i, h, c)
+      Next
+    Next
+
+    'S:Saturation 描画
+    For i As Integer = 0 To pic_s.Width - 1
+
+      Dim value As Integer = CInt(Hsv.Unit(i, pic_s.Width, 255))
+      Dim c As Color = Hsv.HsvToColor(_8bit_val_h, value, 255)
+
+      For h As Integer = 0 To s_bmp.Height - 1
+        s_bmp.SetPixel(i, h, c)
+      Next
+    Next
+
+    'V:Value 描画
+    For i As Integer = 0 To pic_v.Width - 1
+
+      Dim value As Integer = CInt(Hsv.Unit(i, pic_v.Width, 255))
+      Dim c As Color = Hsv.HsvToColor(_8bit_val_h, val_s, value)
+
+      For h As Integer = 0 To v_bmp.Height - 1
+        v_bmp.SetPixel(i, h, c)
+      Next
+    Next
+
+    pic_h.Image = h_bmp
+    pic_s.Image = s_bmp
+    pic_v.Image = v_bmp
+  End Sub
+
+  Private Sub btnOk_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnOk.Click
+
+    _hsb.H = CInt(Hsv.Unit(Val(lbl_H.Text), 255, 360))
+    _hsb.S = lbl_S.Text
+    _hsb.V = lbl_V.Text
+
+    Me.Close()
+  End Sub
+
+  Private Sub btnClose_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnClose.Click
+    _isCancel = True
+    Me.Close()
+  End Sub
+
+
+End Class
+
+
+Public Class Hsv
+
+  Private _H As Integer
+  Private _S As Integer
+  Private _V As Integer
+
+#Region " property "
+
+  '0～360
+  Public Property H()
+    Get
+      Return _H
+    End Get
+    Set(ByVal value)
+      _H = value
+    End Set
+  End Property
+
+  '0～255
+  Public ReadOnly Property H_8bit()
+    Get
+      Return CInt(Unit(_H, 360, 255))
+    End Get
+  End Property
+
+  Public Property S()
+    Get
+      Return _S
+    End Get
+    Set(ByVal value)
+      _S = value
+    End Set
+  End Property
+
+  Public Property V()
+    Get
+      Return _V
+    End Get
+    Set(ByVal value)
+      _V = value
+    End Set
+  End Property
+
+  Public ReadOnly Property R()
+    Get
+      Return HsvToColor(_H, _S, _V).R
+    End Get
+  End Property
+
+  Public ReadOnly Property G()
+    Get
+      Return HsvToColor(_H, _S, _V).G
+    End Get
+  End Property
+
+  Public ReadOnly Property B()
+    Get
+      Return HsvToColor(_H, _S, _V).B
+    End Get
+  End Property
+
+  Public ReadOnly Property RgbColor()
+    Get
+      Return HsvToColor(_H, _S, _V)
+    End Get
+  End Property
+
+
+#End Region
+
+  Shared Sub ColorToHsv(ByVal color As Color, _
+                        ByRef h As Double, ByRef s As Double, ByRef v As Double)
+
+    Dim max As Integer = Math.Max(color.R, Math.Max(color.G, color.B))
+    Dim min As Integer = Math.Min(color.R, Math.Min(color.G, color.B))
+
+    h = color.GetHue()
+
+    If max = 0 Then
+      s = 0
+    Else
+      s = 1 - (min / max)
+    End If
+
+    v = max / 255
+
+  End Sub
+
+  Shared Function HsvToColor(ByVal h As Double, ByVal s As Double, ByVal v As Double) As Color
+
+    Dim hi As Integer = CInt(Math.Floor(h / 60)) Mod 6
+    Dim f As Double = (h / 60) - Math.Floor(h / 60)
+
+    Dim vv, pp, qq, tt As Integer
+    vv = CInt(v)
+    pp = CInt(v * (1 - (s / 255)))
+    qq = CInt(v * (1 - (s / 255) * f))
+    tt = CInt(v * (1 - (s / 255) * (1 - f)))
+
+    'Debug.Print("vv : " & vv & ", pp : " & pp & ", qq : " & qq & ", tt : " & tt)
+
+    Select Case hi
+      Case 0
+        Return Color.FromArgb(255, vv, tt, pp)
+      Case 1
+        Return Color.FromArgb(255, qq, vv, pp)
+      Case 2
+        Return Color.FromArgb(255, pp, vv, tt)
+      Case 3
+        Return Color.FromArgb(255, pp, qq, vv)
+      Case 4
+        Return Color.FromArgb(255, tt, pp, vv)
+      Case Else
+        Return Color.FromArgb(255, vv, pp, qq)
+    End Select
+
+  End Function
+
+  ''' ------------------------------------------------------
+  ''' <summary>単位変換</summary>
+  ''' ------------------------------------------------------
+  Shared Function Unit(ByVal value As Double, ByVal ValueMaxUnit As Double, ByVal ExchangeMaxUnit As Double) As Double
+    Return (value / ValueMaxUnit) * ExchangeMaxUnit
+  End Function
+
+End Class
